275cc72b788483355728ca1f495e831fc986ef13
Ronald <rvpostha15@yahoo.com>
2022-11-04 17:42:51 -0400

    commit tree abb73b99479f30fd3f69fded123daf9c85373610

--- index.js
+++ index.js
@@ -1,1 +1,44 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+beforeEach(function () {
+    cats.length = 0;
+
+    cats.push("Milo", "Otis", "Garfield");
+});
+
+function destructivelyAppendCat() {
+    cats.push("Ralph");
+}
+
+function destructivelyPrependCat() {
+    cats.unshift("Bob");
+}
+
+function destructivelyRemoveLastCat() {
+    cats.pop();
+}
+
+function destructivelyRemoveFirstCat() {
+    cats.shift();
+}
+
+function appendCat() {
+    const appendCat = [...cats, "Broom"];
+    return appendCat;
+}
+
+function prependCat() {
+    const prependCat = ["Arnold", ...cats];
+    return prependCat;
+} 
+
+function removeLastCat() {
+    var removeLastCat = cats.slice(0, cats.length -1);
+    return removeLastCat;
+}
+
+function removeFirstCat() {
+    var removeFirstCat = cats.slice(1);
+    return removeFirstCat;
+}
